e5f0bee50b12fa0f2be5fd389961d5992bd25d30
bierli <david.rejchrt@gmail.com>
2021-11-12 14:22:09 +0100

    Added some documentation to the akig_listener node

--- scripts/akig_listener.py
+++ scripts/akig_listener.py
@@ -8,7 +8,7 @@ from nav_msgs.msg import Odometry
 from sensor_msgs.msg import JointState, Imu
 from geometry_msgs.msg import PoseWithCovarianceStamped, PoseArray
 
-USAGE="""
+USAGE = """
 Usage:
 
 python topic_listener.py topic action
@@ -31,6 +31,12 @@ MSG_TYPES = {
     }
 
 class AkigMessage():
+    """
+    This class stores a message in a dictionary and provides extra functionality
+    to the data, so that one can easily convert the data to various formats.
+
+    #TODO : check for pose arrays...
+    """
     def __init__(self, data):
         self.data = data
         
@@ -55,21 +61,46 @@ class AkigMessage():
     
 
 def flatten_dict(d, parent_key='', sep='_'):
-    # Thanks Imran
-    # https://stackoverflow.com/questions/6027558/flatten-nested-dictionaries-compressing-keys
-    items = []
-    for k, v in d.items():
-        new_key = parent_key + sep + k if parent_key else k
+    """
+    This function removes nested dictionaries from specified dictionary and
+    puts them in the 'top' level. The keys of the nested dictionaries are
+    are prefixed by their parent key.
+
+    Example:
+    {'foo':'bar','baz':{'a':1,'b':8}} => {'foo':'bar','baz.a':1,'baz.b':8}
+
+    param d: dictionary to be flattened
+    param parent_key: key, that contains nested dict
+    param sep: separator that is used in in keys between parents and children
+    
+    Thanks Imran
+    https://stackoverflow.com/questions/6027558/flatten-nested-dictionaries-compressing-keys
+    """
+    items = [] # list containing the keys
+    for k, v in d.items(): # iterate over all items
+        new_key = parent_key + sep + k if parent_key else k # get new key name
+        # check if current value is a nested dict
         if isinstance(v, collections.MutableMapping):
+            # if dict => rescursive call
             items.extend(flatten_dict(v, new_key, sep=sep).items())
         else:
+            # else append the new_key to the items
             items.append((new_key, v))
-    return dict(items)
+    return dict(items) # convert to dict
 
 
 ############################## ROS related methods #############################
 
 def callback(data):
+    """
+    This function specifies what happens when the subscriber node captures a message
+    on the topic it's listening to. In this case the function converts the message
+    to an AkigMessage object that provides addtional functionality (such as convert
+    to CSV etc.) to the data by re-structuring the data.
+
+    param data: captured Message
+    """
+    
     if topic == '/odometry/filtered':
         akigmsg = AkigMessage({
                 'header':{
@@ -110,24 +141,109 @@ def callback(data):
                     'covarinace': data.twist.covariance
                     },
             })
-        print(akigmsg.to_csv())
+        
     elif topic == '/joint_states':
-        pass
+        akigmsg = AkigMessage({
+            'header':{
+                    'seq': data.header.seq,
+                    'stamp': data.header.stamp,
+                    'frame_id':data.header.frame_id
+                    },
+            'name':data.name,
+            'position':data.position,
+            'velocity':data.velocity,
+            'effort':data.effort
+            })
     elif topic == '/amcl_pose':
-        pass
+        akigmsg = AkigMessage({
+            'header':{
+                'seq': data.header.seq,
+                'stamp': data.header.stamp,
+                'frame_id':data.header.frame_id
+                    },
+            'pose':{
+                'pose':{
+                    'position':{
+                        'x': data.pose.pose.position.x,
+                        'y': data.pose.pose.position.y,
+                        'z': data.pose.pose.position.z,
+                        },
+                    'orientation':{
+                        'x': data.pose.pose.orientation.x,
+                        'y': data.pose.pose.orientation.y,
+                        'z': data.pose.pose.orientation.z,
+                        'w': data.pose.pose.orientation.w,
+                        }
+                    },
+                'covarinace':data.pose.covariance
+                },
+            })
     elif topic == '/novatel_data/rawimudata_SIunits':
-        pass
+        akigmsg = AkigMessage({
+            'header':{
+                'seq': data.header.seq,
+                'stamp': data.header.stamp,
+                'frame_id':data.header.frame_id
+                },
+            'orientation':{
+                'x': data.orientation.x,
+                'y': data.orientation.y,
+                'z': data.orientation.z,
+                'w': data.orientation.w,
+                },
+            'orientation_covariance': data.orientation_covariance,
+            'angular_velocity': {
+                'x': data.angular_velocity.x,
+                'y': data.angular_velocity.y,
+                'z': data.angular_velocity.z,
+                },
+            'angular_velocity_covariance': data.angular_velocity_covariance,
+            'linear_acceleration': {
+                'x': data.linear_acceleration.x,
+                'y': data.linear_acceleration.y,
+                'z': data.linear_acceleration.z,
+                },
+            'linear_acceleration_covariance': data.linear_acceleration_covariance
+            
+            })
     elif topic == '/particlecloud':
-        pass
+        akigmsg = AkigMessage({
+            'header':{
+                'seq': data.header.seq,
+                'stamp': data.header.stamp,
+                'frame_id':data.header.frame_id
+                },
+            'poses': [{
+                'position':{
+                    'x': pose.position.x,
+                    'y': pose.position.y,
+                    'z': pose.position.z,
+                    },
+                'orientation':{
+                    'x': pose.orientation.x,
+                    'y': pose.orientation.y,
+                    'z': pose.orientation.z,
+                    'w': pose.orientation.w,
+                }} for pose in data.poses]
+            })
     else:
         # TODO: Catch the exception and print usage
         raise NotImplementedError('Topic not implemented')
 
+    # Work with the akig message object
 
+    print(akigmsg.to_csv())
+
+    # e.g. write to a file or plot the data
+    
+    
         
 def listener():
+    # create the node
     rospy.init_node('akig_listener', anonymous=True)
+    # Subscribe the node to specified topic
     rospy.Subscriber(topic, MSG_TYPES[topic], callback)
+    # Loopy loop
     rospy.spin()
 
 if __name__ == '__main__':
